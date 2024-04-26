075891429358fa1fedc420b15b2a840ea3c3b17a
kyungstar <azure0563@gmail.com>
2024-04-26 23:26:16 +0900

    fastify 작업중

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,17 @@
-import fastify from "fastify";
-import formData from "fastify-formdata";
-import { AddressInfo } from "net";
+// ESM
+import Fastify from 'fastify'
+import formbody from '@fastify/formbody'
 
+const fastify = Fastify({
+    logger: true
+})
 
+fastify.get('/', async (request, reply) => {
+    reply.type('application/json').code(200)
+    return { hello: 'world' }
+})
 
-const app = fastify();
-
-app.register(formData, {
-    limits: {
-        fields: 0,
-        fileSize: 1024 * 1024 * 1024,
-        files: 1,
-    },
-    addToBody: true,
-    sharedSchemaId: "fileSchema",
-});
-
-app.get("/", async (request, reply) => {
-    return reply.status(200).send();
-});
-
-app.head("/", async (request, reply) => {
-    return reply.status(200).send();
-});
-
-// Routing
-// app.register(router, { prefix: "/" });
-
-const start = async () => {
-    try {
-        // await app.listen(Config.SERVER.PORT);
-        const { port } = app.server.address() as AddressInfo;
-    } catch (err) {
-        process.exit(1);
-    }
-};
-
-start();
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) throw err
+    // Server is now listening on ${address}
+})
